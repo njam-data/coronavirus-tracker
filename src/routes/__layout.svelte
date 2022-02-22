462e9f9ee81b08b2eab87b095a492d38e1f968ff
sethvincent <sethvincent@gmail.com>
2022-02-22 08:35:41 -0800

    .

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,9 @@
+<script>
+  import "../app.css";
+</script>
+
+<div class="flex h-screen p-4">
+  <div class="max-w-[400px] m-auto bg-slate-100 p-4 rounded">
+    <slot />
+  </div>
+</div>
---
+++ src/routes/index.svelte
@@ -0,0 +1,27 @@
+<h1 class="text-lg font-bold">Coronavirus dashboard unavailable</h1>
+<p class="text-sm">Our coronavirus dashboard is currently unavailable. For now, please check out these resources:</p>
+<ul class="mt-4 text-sm">
+	<li>
+		<a href="https://www.nj.com/coronavirus/"
+			class="font-semibold p-2 bg-white rounded-sm m-1 block border-l-2 border-300 hover:border-slate-500"
+		>
+			Coronavirus coverage at nj.com
+		</a>
+	</li>
+
+	<li>
+		<a href="https://www.nj.gov/health/cd/topics/covid2019_dashboard.shtml"
+			class="font-semibold p-2 bg-white rounded-sm m-1 block border-l-2 border-300 hover:border-slate-500"
+		>
+			N.J. Department of Health Covid dashboard
+		</a>
+	</li>
+
+	<li>
+		<a href="https://www.nytimes.com/interactive/2021/us/new-jersey-covid-cases.html"
+			class="font-semibold p-2 bg-white rounded-sm m-1 block border-l-2 border-300 hover:border-slate-500"
+		>
+			NYT Covid tracker for New Jersey
+		</a>
+	</li>
+</ul>
