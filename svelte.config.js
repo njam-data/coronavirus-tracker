adf2d0e2825cd48ad2730658a267f7a861a419b0
sethvincent <sethvincent@gmail.com>
2022-02-22 08:48:42 -0800

    fix svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,21 @@
-import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-auto";
+import preprocess from 'svelte-preprocess'
+import adapter from '@sveltejs/adapter-static'
+
+const env = process.env.NODE_ENV
+
+let base = ''
+if (env === 'production') {
+	base = '/coronavirus-tracker'
+}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter(),
+    paths: {
+			base
+		},
+		trailingSlash: 'always'
   },
 
   preprocess: [
